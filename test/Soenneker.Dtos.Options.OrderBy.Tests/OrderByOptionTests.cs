d625974cdd676373f415f20a6257ab7f86c0b7ea
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:40 -0500

    TUnit conversion

--- test/Soenneker.Dtos.Options.OrderBy.Tests/OrderByOptionTests.cs
+++ test/Soenneker.Dtos.Options.OrderBy.Tests/OrderByOptionTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Dtos.Options.OrderBy.Tests;
 
 public sealed class OrderByOptionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
